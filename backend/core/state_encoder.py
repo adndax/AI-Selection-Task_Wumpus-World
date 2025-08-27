3337439ed4c4729df7be1976f241d7ff22f25629
adndax <adindaputri0517@gmail.com>
2025-08-27 14:09:06 +0700

    fix: qtable format

--- backend/core/state_encoder.py
+++ backend/core/state_encoder.py
@@ -4,11 +4,7 @@ class StateEncoder:
         self.state_counter = 0
 
     def encode(self, state):
-        state_tuple = tuple(state)
-        if state_tuple not in self.states_map:
-            self.states_map[state_tuple] = self.state_counter
-            self.state_counter += 1
-        return self.states_map[state_tuple]
+        return tuple(state)
 
     def get_state_space_size(self):
         return self.state_counter
